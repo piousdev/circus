1421092ccddd4b3b3d4a7f8de1e02c07c0b94758
Pious Alpha <102190049+piousdev@users.noreply.github.com>
2024-01-30 14:30:58 +0100

    Add new files and components

---
+++ app/(protected)/playground/page.tsx
@@ -0,0 +1,11 @@
+import Playground from "@/components/code-editor/Playground";
+
+const PlaygroundPage = () => {
+    return (
+        <Playground
+            displayName='placeholderName'
+        />
+    )
+}
+
+export default PlaygroundPage;
